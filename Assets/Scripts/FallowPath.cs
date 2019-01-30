2de9822f497ee7aef09e4494726c3cff29e03aba
pickupst <gd.okanuslu@gmail.com>
2019-01-30 19:05:02 +0300

    Add moving platform on path

---
+++ Assets/Scripts/FallowPath.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallowPath : MonoBehaviour
+{
+    public PathDefinition path;
+    public float speed = 5f;
+    public float maxDistanceToGoal = 0.1f;
+
+
+    IEnumerator<Transform> _currentPoint;
+    float maxDistanceToGoalSQR;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (path == null)
+        {
+            Debug.LogError("Path cannot be null", gameObject);
+            return;
+        }
+
+        _currentPoint = path.GetPathEnumerator();
+        _currentPoint.MoveNext();
+
+        transform.position = _currentPoint.Current.position;
+
+        maxDistanceToGoalSQR = maxDistanceToGoal * maxDistanceToGoal;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_currentPoint == null || _currentPoint.Current == null)
+        {
+            return;
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, _currentPoint.Current.position, Time.deltaTime * speed);
+
+        var distanceSQR = (transform.position - _currentPoint.Current.position).sqrMagnitude;
+        if (distanceSQR < maxDistanceToGoalSQR)
+        {
+            _currentPoint.MoveNext();
+        }
+    }
+}
---
+++ Assets/Scripts/PathDefinition.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathDefinition : MonoBehaviour
+{
+    public Transform[] points;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (points == null && points.Length < 2)
+        {
+            return;
+        }
+
+        for (int i = 1; i < points.Length; i++)
+        {
+            if (points[i - 1] != null)
+            {
+
+                Gizmos.DrawLine(points[i - 1].position, points[i].position);
+
+            }
+        }
+    }
+
+    public IEnumerator<Transform> GetPathEnumerator()
+    {
+
+        var index = 0;
+        var direction = 1;
+
+        while (true)
+        {
+            yield return points[index];
+            if (index <= 0)
+            {
+                direction = 1;
+            }
+            else if (index >= points.Length - 1)
+            {
+                direction = -1;
+            }
+            index += direction;
+        }
+    }
+}
