1907eb88b46f582bd6035d0c3cc85c75eeb2de64
pickupst <gd.okanuslu@gmail.com>
2019-02-05 09:45:20 +0300

    Add player particles system

---
+++ Assets/Scripts/SortParticleSystem.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SortParticleSystem : MonoBehaviour
+{
+
+    public string LayerName = "Particles";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        this.GetComponent<Renderer>().sortingLayerName = LayerName;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
