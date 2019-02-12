a500243ce4f09ca2e27afda5eae9b7f6fd36edab
pickupst <gd.okanuslu@gmail.com>
2019-02-12 20:41:47 +0300

    Add Red Saw

---
+++ Assets/Scripts/PathedProjectile.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathedProjectile : MonoBehaviour
+{
+    private Transform _destination;
+    private float _speed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, _destination.position, Time.deltaTime * _speed);
+
+        var distanceSquared = (_destination.transform.position - transform.position).sqrMagnitude;
+
+        if (distanceSquared > 0.1f * 0.1f)
+        {
+            return;
+        }
+
+        Destroy(gameObject);
+    }
+
+    internal void Initialize(Transform destination, float speed)
+    {
+        _destination = destination;
+        _speed = speed;
+    }
+}
---
+++ Assets/Scripts/PathedProjectileSpawner.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathedProjectileSpawner : MonoBehaviour
+{
+
+    public Transform Destination;
+    public PathedProjectile Projectile;
+    public float speed = 6;
+    public float FireRate = 2f;
+
+    private float _nextShotInSecond;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        _nextShotInSecond = FireRate;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((_nextShotInSecond -= Time.deltaTime) > 0)
+        {
+            return;
+        }
+        _nextShotInSecond = FireRate;
+
+        var projectile = (PathedProjectile)Instantiate(Projectile, transform.position, transform.rotation);
+
+        projectile.Initialize(Destination, speed);
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (Destination == null)
+        {
+            return;
+        }
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, Destination.position);
+    }
+}
