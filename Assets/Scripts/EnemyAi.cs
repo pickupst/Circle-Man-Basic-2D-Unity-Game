f51e0c59dfafec54947692d97de9ab55770d0ec5
pickupst <gd.okanuslu@gmail.com>
2019-02-13 09:42:58 +0300

    Add enemy attack in fire rate

--- Assets/Scripts/EnemyAi.cs
+++ Assets/Scripts/EnemyAi.cs
@@ -5,16 +5,19 @@ using UnityEngine;
 public class EnemyAi : MonoBehaviour
 {
     public float speed = 9f;
+    public float fireRate = 1f;
 
+    public Projectile Projectile;
 
     private CharacterController _controller;
     private Vector2 _direction;
+    private float canFireRate;
 
 
     // Start is called before the first frame update
     void Start()
     {
-
+        canFireRate = fireRate;
         _controller = GetComponent<CharacterController>();
         _direction = new Vector2(-1, 0);
 
@@ -23,6 +26,8 @@ public class EnemyAi : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        canFireRate -= Time.deltaTime;
+
         _controller.SetHorizontalForce(_direction.x * speed);
 
         if ((_direction.x <= 0 && _controller.State.IsCollidingLeft) || (_direction.x >= 0 && _controller.State.IsCollidingRight))
@@ -30,5 +35,13 @@ public class EnemyAi : MonoBehaviour
             _direction = -_direction;
             transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
         }
+
+        if (canFireRate < 0)
+        {
+            var projectile = (Projectile)Instantiate(Projectile, transform.position, transform.rotation);
+            projectile.Initialize(gameObject, _direction, _controller.Velocity);
+            canFireRate = fireRate;
+        }
+        
     }
 }
