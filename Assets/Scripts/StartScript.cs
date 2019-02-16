a366140bc5ec087cf8ad954b3ad4868f840f991d
pickupst <gd.okanuslu@gmail.com>
2019-02-16 07:27:12 +0300

    Add level 2 and start screen

---
+++ Assets/Scripts/StartScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class StartScript : MonoBehaviour
+{
+
+    public string firstLevel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene(firstLevel);
+            //Application.LoadLevel(firstLevel);
+        }
+    }
+}
